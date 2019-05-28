0c20a43f5d72d6a96137562a9883b55ff67a0ae9
bwong365 <bwong365@outlook.com>
2019-05-27 21:52:49 -0600

    fixed a bug affecting sidebar and added a logo

--- src/app/home/components/nav/nav.component.ts
+++ src/app/home/components/nav/nav.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    M.AutoInit();
   }
 
 }
